ed171270df7f89cf43d5dd03f1093210d98a3e82
Jmsantini <jmsantini@live.com>
2019-11-04 16:29:08 -0300

    Exercicios Aula 02 - Logica Booleana

---
+++ Semana1/Aula2LogicaBooleana/script.js
@@ -0,0 +1,95 @@
+/**
+ *
+ * EXERCÍCIOS DE INTERPRETAÇÃO
+ *
+ * EXERCÍCIO 1
+ * a. false
+ * b. false
+ * c. true
+ * d. false
+ * e. boolean
+ * 
+ * 
+ */
+
+/**
+ * EXERCÍCIO 2
+ * a.  Arrays são maneiras de armazenar e acessar mais de uma informação. let array = [1 , 2 , 3 ]
+ * b. 0
+ * c. Usando o comando .lenght 
+ * d. 
+ *I. undefined 
+ *II. null  
+ *III. 11
+ *IV. 3 e 4
+ *V. 19 e 9
+ *VI. 3
+ *VII. 1
+ */
+
+
+/**
+ *
+ * EXERCÍCIOS DE ESCRITA DE CÓDIGO
+ *
+ * EXERCÍCIO 1
+ * a. 
+const fah = 77
+const result1 = (fah - 32)*5/9 + 273.15
+console.log(result1 + "°K")
+*b.
+const cel = 80
+const result2 = (cel)*9/5 + 32
+console.log(result2 + "°F")
+*c
+const cel = 30
+const fah1 = (cel)*9/5+32 
+const kel1 = (fah1 - 32)*5/9 + 273.15
+console.log(fah1 + '°F', kel1 + '°K' )
+
+*d
+const cel = prompt ("Digite a temperatura em Graus °C ")
+const fah1 = (cel)*9/5+32 
+const kel1 = (fah1 - 32)*5/9 + 273.15
+console.log(fah1 + '°F', kel1 + '°K' )
+
+
+ */
+
+/**
+ * EXERCÍCIO 2
+const nome = prompt ("Qual o seu primeiro nome?")
+console.log("Olá "+ nome )
+const snome = prompt ("Qual o seu sobrenome?")
+console.log('Seu nome completo é '+ nome,snome )
+const comida = prompt ("Qual sua comida favorita?")
+console.log("Sua comida favorita é, " + comida)
+const filme = prompt ("Qual seu gênero de filme preferido ?")
+console.log("Seu gênero de filme preferido é, " + filme)
+const serie = prompt ("Qual sua série preferida ?")
+console.log("Sua série prefereida é, " + serie)
+console.log("Obrigado pelas respostas, "+nome + " até mais.")
+
+
+*/
+
+/**
+ * EXERCÍCIO 3
+a.
+const valorQH = 998/5
+console.log("R$"+valorQH+ " Kwh")
+
+b.
+const consumo = valorQH*280
+console.log("Valor da conta é R$:"+consumo)
+
+c.
+const desconto = consumo*0.85
+const  arre = parseFloat(desconto.toFixed(2));
+console.log(arre)
+
+
+
+
+
+ */
