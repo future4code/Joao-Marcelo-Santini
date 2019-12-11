64a31e5a53f2d81ca13758ea2f519c9819631a17
Jmsantini <jmsantini@live.com>
2019-12-11 17:01:28 -0300

    Atividade F4Users, DS (Refeita)

--- Semana7/aula2-integracao/f4users/src/App.js
+++ Semana7/aula2-integracao/f4users/src/App.js
@@ -1,158 +1,41 @@
-import React from 'react';
+import React from "react";
 import './App.css';
-import axios from "axios";
+import UserSignup from "./components/UserSingup/index";
+import UsersList from "./components/UserList/index";
 
-const baseUrl = "https://us-central1-future4-users.cloudfunctions.net/api";
-const token = "santini";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      currentPage: "signup"
+    };
+  }
 
-
-
-
-  class App extends React.Component{
-    
-    constructor(props){
-      super(props);
-      this.state = {
-        cadastroNome:'',
-        cadastroEmail:'',
-        allUsers: [],
-
-      }
+  handleChangePage = () => {
+    if (this.state.currentPage === "signup") {
+      this.setState({ currentPage: "list" });
+    } else {
+      this.setState({ currentPage: "signup" });
     }
-// ..................................Mostrar Cadastros.................................................................
-
-    getAllUsers = () => {
-      const url = `${baseUrl}/users/getAllUsers`;
-     
-
-     const request = axios.get(url,{
-        headers: {
-          "api-token" : token
-        }
-      });
-
-      request.then((response) =>{
-        const allUsers = response.data.result;
-      console.log(response.data.result)
-        this.setState({ allUsers: allUsers });
-      });
-
-      // request.then(response => {
-      //   this.getAllUsers();
-      // }); 
-      // LOOP DOIDão
-
-};
-
-
-
-    // .......................................Controlar inputs, e cadastrar.............................................................
-  
-   onControleNome = (event) => {
-     this.setState({  cadastroNome: event.target.value  })
-   }
-
-   onControleEmail = (event) => {
-     this.setState({  cadastroEmail: event.target.value  })
-   }
-
-   cadastrarPessoa = () => {
-     const url = `${baseUrl}/users/createUser`;
-     
-     const data = {
-       name: this.state.cadastroNome,
-       email: this.state.cadastroEmail
-     }
-     
-     
-     
-    const request = axios.post(url, data,{
-      headers: {
-        "api-token" : token
-      }
-     })
-     
-     request
-      .then((response) => {
-         alert("Cadastrado")
-     })
-      .catch((error) => {
-        alert("Erro");
-    })
-
   };
 
-//............................................Deletar Item...............................
+  render() {
+    // Define o texto do botão baseado na
+    // página em que nos encontramos no momento
+    const buttonText =
+      this.state.currentPage === "signup"
+        ? "Ir para Lista"
+        : "Ir para Cadastro";
 
-  deleteItem = () => {
-    const url = `${baseUrl}/users/deleteUser?id=string`;
-    const request = axios.delete(url,{
-      headers: {
-        "api-token" : token
-      }
-    });
-    
-    request.then((response) =>{
-      const deleteid = response.data.result.id;
-    
-      this.setState({ deleteid: deleteid });
-    });
-    
+    return (
+      <div className="App">
+        <button onClick={this.handleChangePage}>{buttonText}</button>
+
+        {this.state.currentPage === "signup" ? <UserSignup /> : <UsersList />}
+      </div>
+    );
   }
+}
 
-
-
-
-
-
-  // ...................................................................................................
-  
-    render(){
-      return(
-        <div className="App">
-        
-        <button onClick={this.getAllUsers}>Lista de Cadastro</button>
-          <div>
-            <ul>
-             {this.state.allUsers.map(users => (
-             <li key={users.id}>{users.name}<button onClick={() => this.deleteItem(users.id)} >x</button></li>
-             ))}
-            </ul>
-
-          </div>
-        
-
-
-
-        <h2>Cadastro</h2>
-
-
-          <div>
-            <input type="text" 
-            onChange={this.onControleNome} 
-            placeholder="Nome" 
-            value={this.state.cadastroNome}
-            />
-            <br/>
-            <input type="email" 
-            onChange={this.onControleEmail} 
-            placeholder="Email"
-            value={this.state.cadastroEmail} 
-            />
-            <br/>
-            <button onClick={this.cadastrarPessoa}> Cadastrar </button>
-          </div>
-        
-        </div>
-
-
-      );
-    }
-  }
-
-
-
-
-  
 export default App;
---
+++ Semana7/aula2-integracao/f4users/src/components/UserList/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+import UserListItem from "../UserListItem";
+
+const baseURL = "https://us-central1-future4-users.cloudfunctions.net/api";
+
+class UsersList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allUsers: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+
+  getAllUsers = async () => {
+    const axiosConfig = {
+      headers: {
+        "api-token": "santini"
+      }
+    };
+
+
+
+    // Forma síncrona de fazer usando await
+    const response = await axios.get(
+      `${baseURL}/users/getAllUsers`,
+      axiosConfig
+    );
+
+    this.setState({ allUsers: response.data.result });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Usuários Cadastrados</h3>
+        {this.state.allUsers.map(user => (
+          <UserListItem user={user} onDeleteUser={this.getAllUsers} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default UsersList;
