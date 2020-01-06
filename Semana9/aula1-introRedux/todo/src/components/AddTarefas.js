b053b928d38a1b32df1ef55a225d28d12082df85
Jmsantini <jmsantini@live.com>
2020-01-06 16:43:03 -0300

    Feito a action lista de tarefa

---
+++ Semana9/aula1-introRedux/todo/src/components/AddTarefas.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function AddTarefas(props) {
+  return 
+  <div>
+  <input placeholder='Coloque a tarefa'></input> <button>Enviar</button>
+  </div>
+}
---
+++ Semana9/aula1-introRedux/todo/src/components/Filtro.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Filtro(props) {
+  return 
+  <div>
+  <button>Todas</button> <button>Incompletas</button><button>Completas</button>
+  </div>
+}
