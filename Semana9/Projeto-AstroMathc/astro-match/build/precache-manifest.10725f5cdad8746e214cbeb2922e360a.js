f93493d03267193a36b6bfa7259e4de44abe5e08
Jmsantini <jmsantini@live.com>
2020-01-13 15:56:26 -0300

    Up

---
+++ Semana9/Projeto-AstroMathc/astro-match/build/precache-manifest.10725f5cdad8746e214cbeb2922e360a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fe4ccf57c282439cf559af5e0e97b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7788d0f87c91090da634",
+    "url": "/static/js/2.9afeef9c.chunk.js"
+  },
+  {
+    "revision": "e9020ab9ce0edcbce20c",
+    "url": "/static/js/main.80ffa43e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
