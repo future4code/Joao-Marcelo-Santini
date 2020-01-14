6621d610209b3b5742683fb494bb6f5641df397e
Jmsantini <jmsantini@live.com>
2020-01-14 20:23:24 -0300

    Implementando APIS, Inicio.

--- Semana10/aula1-reactrouter/astrotrips-boilerplate/src/reducers/index.js
+++ Semana10/aula1-reactrouter/astrotrips-boilerplate/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import { reducerViagens } from "./viagens";
 
 export const generateReducers = history =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    viagens: reducerViagens,
     // Outros reducers aqui
   });
---
+++ Semana10/aula1-reactrouter/astrotrips-boilerplate/src/reducers/viagens.js
@@ -0,0 +1,15 @@
+const initialState = {
+
+    listaViagens: []
+
+};
+
+
+export const reducerViagens = ( state = initialState, action) => {
+    switch(action.type){
+        case "MOSTRAR_VIAGENS":
+
+        default:
+            return state;
+    }
+}
