a3e24f5df8cbfb57ca4e2aee187b8af8e2276979
Jmsantini <jmsantini@live.com>
2020-01-16 14:34:10 -0300

    CSS HOME

--- Semana10/aula1-reactrouter/astrotrips-boilerplate/src/containers/Home/index.js
+++ Semana10/aula1-reactrouter/astrotrips-boilerplate/src/containers/Home/index.js
@@ -3,23 +3,78 @@ import { connect } from "react-redux";
 import { push, replace, goBack } from "connected-react-router";
 import { routes } from "../Router"
 import TextField from "@material-ui/core/TextField";
-import styled from "styled-components";
+import styled, { StyleSheetManager } from "styled-components";
+import futurex from "../../img/futurex.png";
 
-const MainHome = styled.div`
- 
+
+
+const MainDiv = styled.div`
+display:flex;
+flex-direction: column;
 `;
 
+const Header = styled.div`
+display:flex;
+background-color: white;
+height: 100px;
+width: 100%;
+`;
+
+const LinkHeader = styled.a`
+align-self: center;
+margin: 10px;
+font-weight: bolder;
+cursor: pointer;
+`;
+
+const Logo = styled.img`
+width: 150px;
+height: 100px;
+`;
+
+const MainContent = styled.div`
+width: 100%;
+height: 450px;
+text-align: center;
+align-self: center;
+background-color: #3498DB;
+background-image: linear-gradient(#6BFFBA, #206CE8);
+`;
+
+const FooterHome = styled.div`
+text-align: center;
+height: 50px;
+width: 100%;
+position: fixed;
+bottom: 0px;
+background-color: #206CE8;
+color: white;
+`;
+
+const SpaceGif = styled.div`
+
+`;
 
 
 const Home = props => {
     console.log(props);
     return (
-      <MainHome>
-        <h1> HOME PAGE</h1>
-        <button onClick= {props.goToLogin}>LOGIN/ADM</button>
-        <button onClick= {props.goToViagens}>Viagens</button>
-        <button onClick= {props.goToInscricao}>Inscrição</button>
-      </MainHome>
+      <MainDiv>
+       <Header>
+        <Logo src={futurex} />
+        
+          <LinkHeader onClick= {props.goToLogin}>LOGIN/ADM</LinkHeader>
+          <LinkHeader onClick= {props.goToViagens}>Viagens</LinkHeader>
+          <LinkHeader onClick= {props.goToInscricao}>Inscrição</LinkHeader>
+        </Header>
+        <MainContent>
+        <h2>Viagens Espaciais</h2>
+        <h2>Luais nas estrelas</h2>
+        </MainContent>
+      <FooterHome>
+        FUTUREX Viagens
+      </FooterHome>
+      </MainDiv>
     );
   };
 
