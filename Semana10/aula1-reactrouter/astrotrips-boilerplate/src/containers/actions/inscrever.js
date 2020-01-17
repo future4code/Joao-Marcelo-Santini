e594bcccca1971a9dc348a956a4ade0936d24760
Jmsantini <jmsantini@live.com>
2020-01-17 17:03:45 -0300

    update

--- Semana10/aula1-reactrouter/astrotrips-boilerplate/src/containers/actions/inscrever.js
+++ Semana10/aula1-reactrouter/astrotrips-boilerplate/src/containers/actions/inscrever.js
@@ -1,11 +1,11 @@
-import Axios from "axios"
+// import Axios from "axios"
 
-export const createTrip = (name, age, applicationText, profession, country) => async (dispath) => {
-    const response = await Axios.post("https://us-central1-missao-newton.cloudfunctions.net/futureX/joao/apply") {
-        name: name,
-        age: age,
-        applicationText: applicationText,
-        profession: profession,
-        country: country
-    }
-}
+// export const createTrip = (name, age, applicationText, profession, country) => async (dispath) => {
+//     const response = await Axios.post("https://us-central1-missao-newton.cloudfunctions.net/futureX/joao/apply") {
+//         name: name,
+//         age: age,
+//         applicationText: applicationText,
+//         profession: profession,
+//         country: country
+//     }
+// }
