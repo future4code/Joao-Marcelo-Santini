888456261d7e3d60f3dd1c35cc4ffec361010540
Jmsantini <jmsantini@live.com>
2020-04-02 23:15:30 -0300

    Tudo feito, bug arrumado

--- Semana20/aula4-aws-s3/exercicio1/front-end/src/App.js
+++ Semana20/aula4-aws-s3/exercicio1/front-end/src/App.js
@@ -2,45 +2,41 @@ import React from 'react';
 import styled from "styled-components";
 
 
-const DivBody = styled.div`
-display:flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-`
+// const DivBody = styled.div`
+// display:flex;
+// align-items: center;
+// justify-content: center;
+// flex-direction: column;
+// `
 
 
-const ContentForm = styled.form`
-display: flex;
-flex-direction: column;
-width: 500px;
-height: 200px;
-justify-content: center;
-align-items: center;
-border: solid 1px;
-`
+// const ContentForm = styled.form`
+// display: flex;
+// flex-direction: column;
+// width: 500px;
+// height: 200px;
+// justify-content: center;
+// align-items: center;
+// border: solid 1px;
+// `
 
 
 function App() {
 
 
   return (
-    <DivBody>
-      <h3>Envie um arquivo</h3>
-      <ContentForm 
-        action="http://localhost:3000/uploadFile"
-        method="POST"
-        encType="multpart/form-data"
-      >
-        <input name="arquivo" type="file" />
-        
-        <br/>
-        
-        <button type="submit">Enviar</button>
-      
-      </ContentForm>
+    <div>
+    <h4>Olá! Bora fazer uploads 😉</h4>
+<form
+  action="http://localhost:3000/uploadFile"
+  method="POST"
+  enctype="multipart/form-data"
+>
+  <input name="arquivo" type="file" />
+  <button type="submit">Enviar</button>
+</form>
      
-    </DivBody>
+    </div>
   );
 }
 
