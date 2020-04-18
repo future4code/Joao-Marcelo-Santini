958a9355742f2ebe90a9673d04b0eb6ad8062b11
Jmsantini <jmsantini@live.com>
2020-04-17 22:35:52 -0300

    Back atualizado para o AWS

---
+++ Semana21/f4Tube-front/build/precache-manifest.f339b8dfb8d418145aa83d5e3c954a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932f581167465b485ada53066519ed45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d11b31af02d99a80937",
+    "url": "/static/js/2.f87537d2.chunk.js"
+  },
+  {
+    "revision": "ebc3cc885e97f96323d05e6aed27b202",
+    "url": "/static/js/2.f87537d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e817a61b461a472e162e",
+    "url": "/static/js/main.fc4891fb.chunk.js"
+  },
+  {
+    "revision": "bfe0fc8489887a8768d2",
+    "url": "/static/js/runtime-main.9317f5d9.js"
+  },
+  {
+    "revision": "fe4587d31aad6558bb84e33d26f10dc0",
+    "url": "/static/media/logo.fe4587d3.png"
+  }
+]);
